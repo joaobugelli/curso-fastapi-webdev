49d1eb9b66f15b95f67dac60c1865d0726535409
João Bugelli <joaoalexsb@gmail.com>
2024-06-16 01:38:06 -0300

    iniciando o projeto

---
+++ tests/test_app.py
@@ -0,0 +1,18 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fastapi_webdev.app import app
+
+
+def test_read_root_deve_retornar_ok_e_hello_world():
+
+    # Organização do teste (Arrange)
+    client = TestClient(app)
+
+    # Ação do teste (act)
+    response = client.get('/')
+
+    # Valida se está funcionando (assert)
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'hello world'}
