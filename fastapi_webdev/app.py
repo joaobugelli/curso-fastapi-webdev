7b7cab504402137b11079078e488c7bd9dedb85f
João Bugelli <joaoalexsb@gmail.com>
2024-06-22 19:00:16 -0300

    Define o status e o schema da response

--- fastapi_webdev/app.py
+++ fastapi_webdev/app.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from http import HTTPStatus
+from fastapi_webdev.schemas import Message
 
 app = FastAPI()
 
+var = None
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'hello world'}
